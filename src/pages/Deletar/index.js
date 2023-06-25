05e752dcdf33b32dcf1a40598353528be9312a24
TiagoUniverse <tiagocesar68@gmail.com>
2023-06-25 15:30:48 -0300

    Exclusao e CRUD com sucesso!

--- src/pages/Deletar/index.js
+++ src/pages/Deletar/index.js
@@ -1,56 +1,72 @@
 import React, { useState } from 'react';
-import { Alert, View, ScrollView, Text, Image, Button, StyleSheet, TextInput } from 'react-native';
+import { Alert, View, ScrollView, Text, Button, StyleSheet, TextInput } from 'react-native';
+import { getDatabase, ref, remove } from 'firebase/database';
+import { initializeApp } from 'firebase/app';
+import { get, orderByChild, equalTo } from 'firebase/database';
+import { onValue } from 'firebase/database';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCTXwvVKN7uUQZgfc9wWBRY1ZurfWzsEL4",
+  authDomain: "crud-vinil.firebaseapp.com",
+  projectId: "crud-vinil",
+  storageBucket: "crud-vinil.appspot.com",
+  messagingSenderId: "140079233359",
+  appId: "1:140079233359:web:ddc4d2eb07ac60b63bad3a"
+};
+
+// Inicialize o app Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
 
 export default function Alterar() {
-  const [rioEscolhido, setRioEscolhido] = useState(null);
-  const [nomeRio, setNomeRio] = useState(null)
-  const [endereco, setEndereco] = useState(null)
-  const [latitude, setLatitude] = useState(null)
-  const [longitude, setLongitude] = useState(null)
-  const [potencialhidrogenico, setPotencialHidrogenico] = useState(null)
-  const [oxigeniodissolvido, setOxigenioDissolvido] = useState(null)
-  const [temperatura, setTemperatura] = useState(null)
-  const [condutividade, setCondutividade] = useState(null)
-  const [salinidade, setSalinidade] = useState(null)
-  const [turbidez, setTurbidez] = useState(null)
+  const [cdEscolhido, setCdEscolhido] = useState(null);
+  const [nome, setNome] = useState(null);
+  const [compositor, setCompositor] = useState(null);
+  const [mensagem, setMensagem] = useState(null);
 
+  const deletarCd = () => {
+    if (!cdEscolhido) {
+      Alert.alert('Exclusão', 'Nenhum CD selecionado para exclusão.');
+      return;
+    }
 
-  const deletarRio = () => {
-    const endpoint = `https://aquavitta1.pythonanywhere.com/nalinha/delete/${nomeRio}/${endereco}/${latitude}/${longitude}/${potencialhidrogenico}/${oxigeniodissolvido}/${temperatura}/${condutividade}/${salinidade}/${turbidez}`;
-    console.log(endpoint)
-    console.log(nomeRio)
-    fetch(endpoint)
-      .then(resposta => resposta.json())
-        .catch(() => {
-          Alert.alert('Exclusão', 'Ponto de rio deletado com sucesso!');
-        });
-  }
+    const cdsRef = ref(db, `cds/${cdEscolhido.id}`);
 
-
-  const getRio = ( nomeRio) => {
-    const endpoint = `https://aquavitta1.pythonanywhere.com/leitura/${ nomeRio}`;
-    fetch(endpoint)
-      .then(resposta => resposta.json())
-      .then(json => {
-        const rio = {
-          nomeRio: json.rio,
-          endereco: json.endereco,
-          condutividade: json.condutividade,
-          latitude: json.latitude,
-          longitude: json.longitude,
-          oxigeniodissolvido: json.oxigeniodissolvido,
-          potencialhidrogenico: json.potencialhidrogenico,
-          salinidade: json.salinidade,
-          temperatura: json.temperatura,
-          turbidez: json.turbidez
-        };
-        setRioEscolhido(rio);
+    remove(cdsRef)
+      .then(() => {
+        Alert.alert('Exclusão', 'CD excluído com sucesso!');
+        setCdEscolhido(null);
+        setCompositor(null);
+        setMensagem(null);
       })
       .catch(() => {
-        Alert.alert('Erro', 'Não foi possível encontrar este ponto de rio');
+        Alert.alert('Exclusão', 'Não foi possível excluir o CD.');
       });
-  }
+  };
+
+  const buscarCd = (nome) => {
+    const cdsRef = ref(db, 'cds');
+
+    onValue(cdsRef, (snapshot) => {
+      const cds = snapshot.val();
+
+      if (cds) {
+        const cdEncontrado = Object.entries(cds).find(([id, cd]) => cd.nome === nome);
+
+        if (cdEncontrado) {
+          const [id, cd] = cdEncontrado;
+          setCdEscolhido({ id, ...cd });
+        } else {
+          setMensagem('Não foi encontrado nenhum CD com este nome.');
+          setCdEscolhido(null);
+        }
+      } else {
+        setMensagem('Não há CDs cadastrados.');
+        setCdEscolhido(null);
+      }
+    });
+  };
+  
 
   return (
     <View style={styles.container}>
@@ -58,37 +74,37 @@ export default function Alterar() {
         <View style={styles.topo}>
           <Text style={styles.topoTitulo}>AquaVitta</Text>
         </View>
-        {rioEscolhido != null && (
+
+        {cdEscolhido != null && (
           <View style={styles.Box}>
-            <Text style={styles.linha}>Nome do rio: {rioEscolhido.nomeRio}</Text>
-            <Text style={styles.linha}>Endereço: {rioEscolhido.endereco}</Text>
-            <Text style={styles.linha}>condutividade: {rioEscolhido.condutividade}</Text>
-            <Text style={styles.linha}>Latitude: {rioEscolhido.latitude}</Text>
-            <Text style={styles.linha}>Longitude: {rioEscolhido.longitude}</Text>
-            <Text style={styles.linha}>Oxigênio dissolvido: {rioEscolhido.oxigeniodissolvido}</Text>
-            <Text style={styles.linha}>Potencial hidrogênico: {rioEscolhido.potencialhidrogenico}</Text>
-            <Text style={styles.linha}>Salinidade: {rioEscolhido.salinidade}</Text>
-            <Text style={styles.linha}>temperatura: {rioEscolhido.temperatura}</Text>
-            <Text style={styles.linha}>turbidez: {rioEscolhido.turbidez}</Text>
+            <Text style={styles.linha}>Nome do CD: {cdEscolhido.nome}</Text>
+            <Text style={styles.linha}>Compositor: {cdEscolhido.compositor}</Text>
           </View>
         )}
 
-
         <View style={styles.cardContainer}>
-          {rioEscolhido == null && (
-            <><><Text>Digite o nome do rio no campo abaixo:</Text></><TextInput style={styles.TextInputt}
-              onChangeText={setNomeRio}
-              value={nomeRio}
-              KeyboardType="text" /><Button title="Selecionar um rio para exclusão" onPress={() => getRio(nomeRio)} /></>
+          {cdEscolhido == null && (
+            <>
+              <Text>Digite o nome do CD no campo abaixo:</Text>
+              <TextInput
+                style={styles.TextInputt}
+                onChangeText={setNome}
+                value={nome}
+                keyboardType="text"
+                placeholder="Digite o nome do CD"
+              />
+              <Button title="Selecionar um CD para exclusão" onPress={() => buscarCd(nome)} />
+            </>
           )}
 
-          {rioEscolhido != null && (
-            <><><Text>Confira o ponto de rio que vai excluir e confirme no botão abaixo</Text></>
-            <Button  title="Excluir" onPress={() => deletarRio()}></Button></>
+          {cdEscolhido != null && (
+            <>
+              <Text>Confira o CD que deseja excluir e confirme no botão abaixo:</Text>
+              <Button title="Excluir" onPress={() => deletarCd()} />
+            </>
           )}
-
         </View>
-
+        {mensagem && <Text style={styles.mensagem}>{mensagem}</Text>}
       </ScrollView>
     </View>
   );
@@ -96,22 +112,19 @@ export default function Alterar() {
 
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: '#fff' },
-
   topo: { height: 80, padding: 20, paddingTop: 40, marginBottom: 20, backgroundColor: '#00008B' },
   topoTitulo: { fontSize: 22, marginBottom: -10, color: '#fff', textAlign: 'center' },
-
   cardContainer: { borderWidth: 1, borderColor: '#d5d5d5', borderRadius: 4, marginBottom: 10, marginHorizontal: 20, padding: 10 },
-  cardTitle: { fontSize: 22, marginBottom: 20, textAlign: 'center', color: '#656565' },
-
   Box: { alignItems: 'center' },
   linha: { fontSize: 18 },
-  TextInputt:{
+  mensagem: { fontSize: 16, color: 'red', marginBottom: 10, textAlign: 'center' },
+  TextInputt: {
     backgroundColor: '#DCDCDC',
     borderRadius: 4,
     marginBottom: 15,
     marginTop: 5,
     paddingLeft: 10,
     color: '#4F4F4F'
-},
+  },
+  mensagem: { color: 'red', textAlign: 'center', marginTop: 10 }
 });
-
